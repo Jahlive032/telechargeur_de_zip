7eb1461af06ef44c380cd17f205e0aa13ad91378
Lawson Laurenne <lawsondjeckylaurenne@gmail.com>
2024-10-21 17:05:17 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -62,6 +62,7 @@ function App() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
+        <p>Bonjour</p>
         <button
           onClick={handleDownload}
           disabled={isLoading || !url}
